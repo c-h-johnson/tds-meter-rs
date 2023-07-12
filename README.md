e9c542f6b450fc382f84c9c2fb29f7f2bd683b4f
Charles Johnson <charles@charlesjohnson.name>
2023-07-12 22:59:15 +0100

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # tds-meter-rs
-Rust library for TDS Meter based on embedded-hal 
+
+[![](https://badgers.space/badge/crates.io/tds-meter)](https://crates.io/crates/tds-meter)
+
+An embedded-hal driver for TDS Meter (total disolved solids sensor)
+
+# Device
+
+Links:
+
+- [product page](https://whadda.com/product/tds-total-dissolved-solids-water-quality-sensor-wpm356/)
+- [manual](https://cdn.velleman.eu/downloads/25/prototyping/manual_wpm356.pdf)
+
+Also available under different brands.
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,99 @@
+#![no_std]
 
+use core::marker::PhantomData;
+
+use embedded_hal::{
+    adc::{Channel, OneShot},
+    blocking::delay::DelayMs,
+};
+// for f32::powi
+use num_traits::float::FloatCore;
+
+const NO_SAMPLES: usize = 30;
+const SAMPLE_INTERVAL_MS: u16 = 40;
+
+pub enum Error<AdcError> {
+    ReadError(AdcError),
+}
+
+fn median(mut data: [u16; NO_SAMPLES]) -> u16 {
+    data.sort_unstable();
+    return data[data.len() / 2];
+}
+
+pub struct TdsMeter<OneShotReader, Adc, Word, PinData, Timer>
+where
+    OneShotReader: OneShot<Adc, Word, PinData>,
+    PinData: Channel<Adc>,
+    Timer: DelayMs<u16>,
+{
+    adc: OneShotReader,
+    adc_range: u16,
+    adc_vref: f32,
+    pin_data: PinData,
+    timer: Timer,
+    _unused: PhantomData<Adc>,
+    _unused2: PhantomData<Word>,
+}
+
+impl<OneShotReader, Adc, Word, PinData, Timer> TdsMeter<OneShotReader, Adc, Word, PinData, Timer>
+where
+    Word: Into<u16>,
+    OneShotReader: OneShot<Adc, Word, PinData>,
+    PinData: Channel<Adc>,
+    Timer: DelayMs<u16>,
+{
+    pub fn new(
+        adc: OneShotReader,
+        adc_range: u16,
+        adc_vref: f32,
+        pin_data: PinData,
+        timer: Timer,
+    ) -> Self {
+        Self {
+            adc,
+            adc_range,
+            adc_vref,
+            pin_data,
+            timer,
+            _unused: PhantomData,
+            _unused2: PhantomData,
+        }
+    }
+
+    /// Output TDS value in parts per million
+    ///
+    /// Set temperature to the temperature of the water in °C or 25 if unsure.
+    pub fn sample(&mut self, temperature: f32) -> Result<f32, Error<OneShotReader::Error>> {
+        let mut data: [u16; NO_SAMPLES] = [0; NO_SAMPLES];
+        for i in 0..NO_SAMPLES {
+            loop {
+                let read_result = self.adc.read(&mut self.pin_data);
+
+                match read_result {
+                    Ok(word) => {
+                        data[i] = word.into();
+                        break;
+                    }
+                    Err(nb::Error::Other(failed)) => {
+                        return Err(Error::ReadError(failed));
+                    }
+                    Err(nb::Error::WouldBlock) => continue,
+                };
+            }
+
+            self.timer.delay_ms(SAMPLE_INTERVAL_MS);
+        }
+
+        let average_data = median(data);
+        let voltage = (average_data as f32 / self.adc_range as f32) * self.adc_vref;
+
+        // temperature compensation
+        let compensation_coefficient = 1.0 + 0.02 * (temperature - 25.0);
+        let compensated_voltage = voltage / compensation_coefficient;
+
+        Ok((66.71 * compensated_voltage.powi(3))
+            + (-112.93 * compensated_voltage.powi(2))
+            + (428.695 * compensated_voltage))
+    }
+}
